8d5d81f6f0b47dc4663adf40eff4826643bac885
ahmedos1999 <ah_os5@yahoo.com>
2023-06-23 01:38:28 +0200

    alx-low_level_programming

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Prints the largest prime factor of the number 612852475143
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+long i;
+long n = 612852475143;
+double sq = sqrt(n);
+	while (n % 2 == 0)
+	{
+		n /= 2;
+	}
+	while (n % 3 == 0)
+	{
+		n /= 3;
+	}
+	while (n % 7 == 0)
+	{
+		n /= 2;
+	}
+	for (i = 5; i <= sq; i += 6)
+	{
+		while (n % i == 0)
+		{
+			n /= i;	
+		}
+	}
+	printf("%ld \n", n);
+return (0);
+}
